b1460cea957d852833ff6d9ea462f89ab651fa8d
Mark Manipula <markmikomanipula@gmail.com>
2024-11-23 12:12:38 -0800

    [Arrays] Implement flood fill

--- src/com/prep/interview/arrays/Arrays.java
+++ src/com/prep/interview/arrays/Arrays.java
@@ -5,6 +5,27 @@ import java.util.List;
 
 public class Arrays {
 
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) return image;
+
+        dfs(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
+
+    private void dfs(int[][] image, int sr, int sc, int color, int newColor) {
+        //handle edge cases
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) {
+            //do nothing
+            return;
+        }
+        //fill
+        image[sr][sc] = newColor;
+        dfs(image, sr + 1, sc, color, newColor);
+        dfs(image, sr - 1, sc, color, newColor);
+        dfs(image, sr, sc + 1, color, newColor);
+        dfs(image, sr, sc - 1, color, newColor);
+    }
+
     //binary search
     public int search(int[] nums, int target) {
         int left = 0;
